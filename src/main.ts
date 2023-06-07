fbab6f439373a687556e99024e8d0576703789c9
Amir khan <webmasteramir1@gmail.com>
2023-06-08 00:06:14 +0530

    added main ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { RouterModule } from '@angular/router';
 import { routes } from './app/routes';
+import { HttpClientModule } from '@angular/common/http';
 
 if (environment.production) {
   enableProdMode();
@@ -11,8 +12,13 @@ if (environment.production) {
 
 // Bootstrap using component
 bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(RouterModule.forRoot(routes))],
+  providers: [
+    importProvidersFrom(RouterModule.forRoot(routes), HttpClientModule),
+  ],
 }).catch((err) => console.error(err));
+
+// ANgular 15 HttpClientModule can be replaced by
+// providers: [provideHttpClient()]
 
 // Bootstrap using module
 // platformBrowserDynamic()
